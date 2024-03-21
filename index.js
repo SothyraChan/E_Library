d1a0fc062e58d374bf1672f032f77c313bfa2086
SothyraChane <juujuu.q.cb@gmail.com>
2024-03-21 06:48:55 -0400

    Debugging by adding the backticks around the string

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const app = express();
 const port = 3000;
 app.use(express.json()); //middleware
 app.get('/', (req, res) => res.send('E-Library')); //routes
-app.listen(port, () => console.log('E-Library is listening at http://localhost:${port}'));
+app.listen(port, () => console.log(`E-Library is listening at https://localhost:${port}`));
