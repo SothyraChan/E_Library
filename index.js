e2505e6a38cb49e783e9e01e54085be29ee1a697
SothyraChane <juujuu.q.cb@gmail.com>
2024-03-22 19:47:00 -0400

    adjusting the file path to meet the project

--- index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-app.use(express.json()); //middleware
-app.get('/', (req, res) => res.send('E-Library')); //routes
-app.listen(port, () => console.log(`E-Library is listening at https://localhost:${port}`));
