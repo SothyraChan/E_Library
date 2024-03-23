dd5dd48000ca4935ae782f65046f026be73bf377
SothyraChane <juujuu.q.cb@gmail.com>
2024-03-22 23:34:03 -0400

    middleware installation and script additions

--- server.js
+++ server.js
@@ -13,10 +13,3 @@ app.get("/api/v1", (req, res) => {
 app.get("/*", (_req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 })
-const { PORT = 5000 } = process.env;
-app.listen(PORT, () => {
-  console.log();
-  console.log(`  App running in port ${PORT}`);
-  console.log();
-  console.log(`  > Local: \x1b[36mhttp://localhost:\x1b[1m${PORT}/\x1b[0m`);
-});
