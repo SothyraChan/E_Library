3c530858b3a31509d164833c94f189f8f3ca82c2
SothyraChane <juujuu.q.cb@gmail.com>
2024-03-22 23:45:29 -0400

    added some changes

--- server/express.js
+++ server/express.js
@@ -4,7 +4,11 @@ import cookieParser from 'cookie-parser'
 import compress from 'compression'
 import cors from 'cors'
 import helmet from 'helmet'
+import Template from './../template.js'
 const app = express()
+app.get('/', (req, res) => {
+    res.status(200).send(Template()) 
+    })    
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
