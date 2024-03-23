3e8e48f00f9c33b21735bbcf61ebb013e5192b95
SothyraChane <juujuu.q.cb@gmail.com>
2024-03-22 20:03:24 -0400

    Editing for better file path to meet the project requirements

---
+++ elibrary/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: "./src/main.jsx",
+    },
+  },
+});
