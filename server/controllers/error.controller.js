4278ea393a1dbb21f39e865f7144eb3391766e59
Pedro Lucas <plucasmsc@gmail.com>
2024-03-23 19:54:37 -0400

    AUTHENTICATION CHANGES
    My authentication configs. same as in class

---
+++ server/controllers/error.controller.js
@@ -0,0 +1,13 @@
+function handleError(req, res) {
+    // Your code to handle the error
+    }
+    function getErrorMessage(errMsg) {
+    console.log(errMsg);
+    }
+    // Export the controller function
+    export default {
+     handleError: handleError,
+     getErrorMessage:getErrorMessage
+    };
+    
+    
