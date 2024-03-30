4961047b485da70a4cf544f2924402caf0bdd73d
Pedro Lucas <plucasmsc@gmail.com>
2024-03-30 02:29:48 -0400

    Created the profile options

--- elibrary/core/Menu.jsx
+++ elibrary/core/Menu.jsx
@@ -6,21 +6,21 @@ import IconButton from '@material-ui/core/IconButton'
 import HomeIcon from '@material-ui/icons/Home'
 import Button from '@material-ui/core/Button'
 import auth from './../auth/auth-helper'
-import {Link, withRouter} from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 
 const isActive = (history, path) => {
-  if (history.location.pathname == path)
-    return {color: '#bef67a'}
+  if (history.location.pathname === path)
+    return { color: '#bef67a' }
   else
-    return {color: '#ffffff'}
+    return { color: '#ffffff' }
 }
 const isPartActive = (history, path) => {
   if (history.location.pathname.includes(path))
-    return {color: '#bef67a'}
+    return { color: '#bef67a' }
   else
-    return {color: '#ffffff'}
+    return { color: '#ffffff' }
 }
-const Menu = withRouter(({history}) => (
+const Menu = withRouter(({ history }) => (
   <AppBar position="static">
     <Toolbar>
       <Typography variant="h6" color="inherit">
@@ -29,43 +29,45 @@ const Menu = withRouter(({history}) => (
       <div>
         <Link to="/">
           <IconButton aria-label="Home" style={isActive(history, "/")}>
-            <HomeIcon/>
+            <HomeIcon />
           </IconButton>
         </Link>
         <Link to="/books/all">
           <Button style={isActive(history, "/books/all")}>Books</Button>
-        </Link>  
+        </Link>
+        {auth.isAuthenticated() && (
+          <Link to="/profile">
+            <Button style={isActive(history, "/profile")}>Profile</Button>
+          </Link>
+        )}
         <Link to="/books/create">
           <Button style={isActive(history, "/books/create")}>Create Book</Button>
-        </Link>  
+        </Link>
       </div>
-      <div style={{'position':'absolute', 'right': '10px'}}><span style={{'float': 'right'}}>
-      {
-        !auth.isAuthenticated() && (<span>
-          <Link to="/signup">
-            <Button style={isActive(history, "/signup")}>Sign up
-            </Button>
-          </Link>
-          <Link to="/signin">
-            <Button style={isActive(history, "/signin")}>Sign In
-            </Button>
-          </Link>
-        </span>)
-      }
-      {
-        auth.isAuthenticated() && (<span>
-
-          {auth.isAuthenticated().user.admin &&  <Link to="/books/create">
-          <Button style={isPartActive(history, "/books/create")}>Create Book</Button>
-          </Link>}
-
-          <Button color="inherit" onClick={() => {
-              auth.clearJWT(() => history.push('/'))
-            }}>Sign out</Button>
-        </span>)
-
-      }
-      </span></div>
+      <div style={{ 'position': 'absolute', 'right': '10px' }}>
+        <span style={{ 'float': 'right' }}>
+          {!auth.isAuthenticated() && (
+            <span>
+              <Link to="/signup">
+                <Button style={isActive(history, "/signup")}>Sign up</Button>
+              </Link>
+              <Link to="/signin">
+                <Button style={isActive(history, "/signin")}>Sign In</Button>
+              </Link>
+            </span>
+          )}
+          {auth.isAuthenticated() && (
+            <span>
+              {auth.isAuthenticated().user.admin && <Link to="/books/create">
+                <Button style={isPartActive(history, "/books/create")}>Create Book</Button>
+              </Link>}
+              <Button color="inherit" onClick={() => {
+                auth.clearJWT(() => history.push('/'))
+              }}>Sign out</Button>
+            </span>
+          )}
+        </span>
+      </div>
     </Toolbar>
   </AppBar>
 ))
