4ea0450cdd5a906208a54a397c0b33838d39a20e
CRIMEMASTERGOGO/Vidit <vrawat2@my.centennialcollege.ca>
2024-03-31 21:40:50 -0400

    add UI for book details

--- elibrary/books/BookDetails.jsx
+++ elibrary/books/BookDetails.jsx
@@ -19,7 +19,30 @@ const BookComponent = () => {
 
   return (
     <main style={{ width: '100vw', height: '100vh', backgroundColor: 'rgb(189, 200, 220)' }}>
+      <div className="container" style={{ width: '80%', backgroundColor: 'white', borderRadius: '15px', padding: '8px', margin: 'auto', boxShadow: '0 0 10px rgb(115, 115, 115)' }}>
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+          <img src={book.bookPic} alt="Bookicon" style={{ width: '100px', borderRadius:'10px' }} />
+          <div className="book-details" style={{ marginLeft: '15px' }}>
+            <h4 id="book-name">{book.bookName}</h4>
+            <p id="price">Price: {book.price}</p>
+            <p id="author">Author: {book.author}</p>
+            <p id="date-pub">Publication Date: {book.publicationDate}</p>
+          </div>
+        </div>
+        <div>
+          <p>In the mystical land of Eliria, where the sun kisses the sky and the
+            moon whispers secrets to the stars, lies the tale of
+            "{book.bookName}." Within the ancient pages of this enchanted
+            book, one discovers the untold stories of heroes and heroines, whose
+            bravery echoes through the ages.
 
+            From the sprawling forests of Eldoria to the towering peaks of Mount
+            Celestia, the adventures unfold like petals of a blooming flower, each
+            chapter revealing a new mystery waiting to be unraveled. Through
+            perilous journeys and daring escapades, the characters navigate
+            treacherous paths in pursuit of truth and justice.</p>
+        </div>
+      </div>
     </main>
   );
 }
