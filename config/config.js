47253449c1dc0f9bf5c8097ca0d7d60e7d7fc01b
jpablo9988 <65927907+jpablo9988@users.noreply.github.com>
2024-04-09 23:35:34 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const config = {
-    env: process.env.NODE_ENV || 'development', 
-    port: process.env.PORT || 3000,
-    jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", 
+    env: "development",
+    port: 3000,
+    jwtSecret: "YOUR_secret_key",
     mongoUri: process.env.MONGODB_URI || 
     "mongodb+srv://rojindayani:ElectronicLibrary@atlascluster.gr8sruq.mongodb.net/?retryWrites=true&w=majority"
     }
