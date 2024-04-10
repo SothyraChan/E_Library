831a13eea2fa45c675467ebd9f9dc9cd14fa4dbe
jpablo9988 <65927907+jpablo9988@users.noreply.github.com>
2024-04-09 20:38:42 -0400

    matching config.js to profs example

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const config = {
     env: process.env.NODE_ENV || 'development', 
-    port: process.env.PORT || 3000,
+    port: 3000,
     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", 
     mongoUri: process.env.MONGODB_URI || 
     "mongodb+srv://rojindayani:ElectronicLibrary@atlascluster.gr8sruq.mongodb.net/?retryWrites=true&w=majority"
